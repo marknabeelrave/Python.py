f36553b53628fce32425bfd48ac2501b4d421d28
marknabeelrave <75925097+marknabeelrave@users.noreply.github.com>
2025-06-25 10:01:41 +0530

    Add files via upload

---
+++ calculation.py
@@ -0,0 +1,5 @@
+from math import *
+n=int(input("Enter a number:"))
+print("The square root of the number is:",sqrt(n))
+print("The logarithmic value is:", log(n))
+print("The sine value is:", sin(n))
